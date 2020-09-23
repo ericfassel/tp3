791578be5587bc37e96e554e9831906ab9d508ee
ericfassel <71649003+ericfassel@users.noreply.github.com>
2020-09-23 11:02:59 +0200

    Update go.bat

--- go.bat
+++ go.bat
@@ -3,4 +3,4 @@ REM call mvn scm:checkout
 REM cd target/checkout
 call mvn  package
 REM cd ../..
-copy C:\formationmaven\tps\tp3\monappli\monappli-web\target\monappli-web.war  C:\formationmaven\tps\tp3\tomcat\apache-tomcat-8.5.58\webapps
+copy C:\Users\orsys\.jenkins\workspace\job3_plus_bat\monappli-web\target\monappli-web.war  C:\formationmaven\tps\tp3\tomcat\apache-tomcat-8.5.58\webapps
